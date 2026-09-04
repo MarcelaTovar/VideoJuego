604ebab6b027ea990e35b9e1778a3355dd9fe781
Marcela Tovar <marcelatovar.flores@gmail.com>
2026-09-04 10:49:45 -0600

    arreglo de on trigger

--- Assets/Samples/Basketball_target_area.cs
+++ Assets/Samples/Basketball_target_area.cs
@@ -5,7 +5,7 @@ using  XRMultiplayer.MiniGames;
 public class Basketball_target_area : MonoBehaviour
 {
     public Minigame_Basketball basketballManager;
-    public void onTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
